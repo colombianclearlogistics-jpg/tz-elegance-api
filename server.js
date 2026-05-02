231e0330ad4f9c54f0696cf55d3da8b190174ba7
colombianclearlogistics-jpg <colombianclearlogistics@gmail.com>
2026-05-02 09:18:43 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express = require(“express”);
-const fetch = require(“node-fetch”);
-const cors = require(“cors”);
+const express = require(´´express´´);
+const fetch = require(´´node-fetch´´);
+const cors = require(´´cors´´);
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const SCRIPT_URL = “https://script.google.com/macros/s/AKfycbx-sU5HKodKEOxN81WdJ6cLQkiyXiv6Ni2DivvTMHG3ni3ZzdwXXZSaoML43L0-ixwF/exec”;
+const SCRIPT_URL = ´´https://script.google.com/macros/s/AKfycbx-sU5HKodKEOxN81WdJ6cLQkiyXiv6Ni2DivvTMHG3ni3ZzdwXXZSaoML43L0-ixwF/exec´´;
 
-app.get(”/getCitas”, async (req, res) => {
+app.get(´´/getCitas´´, async (req, res) => {
 try {
-const r = await fetch(SCRIPT_URL + “?action=getCitas”);
+const r = await fetch(SCRIPT_URL + ´´?action=getCitas´´);
 const data = await r.json();
 res.json(data);
 } catch (e) {
@@ -18,13 +18,13 @@ res.json({ error: e.message });
 }
 });
 
-app.post(”/cita”, async (req, res) => {
+app.post(´´/cita´´, async (req, res) => {
 try {
 const r = await fetch(SCRIPT_URL, {
-method: “POST”,
-headers: { “Content-Type”: “application/json” },
+method: ´´POST´´,
+headers: { ´´Content-Type´´: ´´application/json´´ },
 body: JSON.stringify(req.body),
-redirect: “follow”
+redirect: ´´follow´´
 });
 const data = await r.json();
 res.json(data);
@@ -33,7 +33,7 @@ res.json({ error: e.message });
 }
 });
 
-app.get(”/”, (req, res) => res.send(“TZ Elegance API OK”));
+app.get(´´/´´, (req, res) => res.send(´´TZ Elegance API OK´´));
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(“Servidor en puerto “ + PORT));
+app.listen(PORT, () => console.log(´´Servidor en puerto ´´ + PORT));
